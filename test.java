4e34e42e4993eb0208842c641fbdd188ec0062ca
Abdelrahman <bedoos2005@gmail.com>
2025-05-07 19:23:29 +0300

    20220321

--- test.java
+++ test.java
@@ -1,6 +1,9 @@
+// github task
+
 class java {
     public static void main(String[] args) {
-        System.out.println(20230211);
-        System.out.println(20230100);
+        System.out.println(20230211);  // first  commit
+        System.out.println(20230100);  // second commit
+        System.out.println(20220321);  // third  comiit
     }
 }
